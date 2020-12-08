12a7255c0852f9fc33eae8234b4951062c33bcb6
Your Name <midinfotn401@gmail.com>
2020-12-08 13:00:17 +0100

    25 last digit check task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if x > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(number, x))
 elif x == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, x))
-elif x < 6 & x != 0:
+elif x < 6 and x != 0:
     print("Last digit of {:d} is {:d}".format(number, x))
