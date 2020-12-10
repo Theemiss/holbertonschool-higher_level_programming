3828c156670b5df4bc338585dfef62891aefb9f9
Your Name <midinfotn401@gmail.com>
2020-12-10 20:40:26 +0100

    magic calc

---
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
