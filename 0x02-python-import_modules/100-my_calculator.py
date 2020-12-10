ee3c5675cddc5dcbc429a238269e318361ff0319
Your Name <midinfotn401@gmail.com>
2020-12-10 20:13:05 +0100

    calcullator task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,24 +4,22 @@ import sys
 
 
 def main(*argv):
-    l = len(sys.argv)
-    if l != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if sys.argv[2] == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
+        if sys.argv[2] == '+':
+            print('{} + {} = {}'.format(a, b, add(a, b)))
+        elif sys.argv[2] == '-':
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+        elif sys.argv[2] == '*':
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
+        elif sys.argv[2] == '/':
+            print('{} / {} = {}'.format(a, b, div(a, b)))
         else:
-            print("Unknown operator. Available operators: +, -, * and /")
+            print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
-
 if __name__ == "__main__":
     main()
