f4f6537fcf74a69cb337773533752a82e78c5f4e
Your Name <midinfotn401@gmail.com>
2020-12-10 18:35:46 +0100

    TASK 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,6 +5,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    print ("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
 if __name__ == "__main__":
     main()
