8aeae059653f4d1d5333ef1c8e757c87862f1266
Your Name <midinfotn401@gmail.com>
2020-12-15 21:56:15 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return (None)
+    else:
+        m = my_list[0]
+        for x in range(1, len(my_list)):
+            if my_list[x] > m:
+                m = my_list[x]
+                return (m)
+        return (m)
