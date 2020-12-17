acf631c27a96d9b43480dca3a98f998ef38520f6
Your Name <midinfotn401@gmail.com>
2020-12-17 20:29:23 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = list(my_list)
-    for x in range(0, len(new) - 1):
-        if new[x] == search:
-            new[x] = replace
+    new = []
+    for x in my_list:
+        if x == search:
+            new.append(replace)
+        else:
+            new.append(x)
     return (new)
