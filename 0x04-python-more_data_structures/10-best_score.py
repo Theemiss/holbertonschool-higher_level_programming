c07afeca89f51413998f538a986d0d625a9ee5dc
Your Name <midinfotn401@gmail.com>
2020-12-17 21:28:27 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    m = 0
+    b = ''
     if a_dictionary is None:
         return None
-    m = 0
-    for k, v in a_dictionary.items():
-        if v > m:
-            res = k
-            m = v
-    return res
+    for i in a_dictionary.keys():
+        if a_dictionary[i] > m:
+            m = a_dictionary[i]
+            b = i
+    return b
