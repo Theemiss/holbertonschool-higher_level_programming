7b0b692fe8fd90723df9db86868ca427a209b970
Your Name <midinfotn401@gmail.com>
2021-01-05 20:10:37 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module Sqaure """
+
+
+class Square:
+    """ Square class defined by geometric shap
+
+        Attributes:
+            size (int): Size of square
+    """
+    def __init__(self, size):
+        """
+        Initialize methode
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
