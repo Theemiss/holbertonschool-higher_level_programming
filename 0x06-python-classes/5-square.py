4c45468183083fbf92fc6ac8dc1362e7e096451b
Your Name <midinfotn401@gmail.com>
2021-01-05 20:57:15 +0100

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" Module Sqaure """
+
+
+class Square:
+    """ Square class defined by geometric shap
+
+        Attributes:
+            size (int): Size of square
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Raises:
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        set square square area
+
+        Return:
+            the current square area (int)
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        getter of size
+
+        Return:
+            Size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter of size
+
+        Args:
+            size (int): size of a side of the square
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """
+        print a square from the size using ##
+
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.size):
+                    print("#" * self.size)
