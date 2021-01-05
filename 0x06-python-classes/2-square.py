c000bfeb618bcce9e534eefc1892c6d40078a199
Your Name <midinfotn401@gmail.com>
2021-01-05 20:19:53 +0100

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Module Sqaure """
+
+
+class Square:
+    """ Square class defined by geometric shap
+
+        Attributes:
+            size (int): Size of square
+    """
+    def __init__(self, size=0):
+        """
+        Initialize methode
+
+        Args:
+            size (int): size of square
+        """
+        self.__set(size)
+
+    def __get(self):
+        """
+        getter of instance
+        """
+        return self.__size
+
+    def __set(self, size):
+        """
+        setter of instance
+
+        Args:
+            size (int): int to assign to square size
+        Raises:
+            TypeError: if size is not int
+            ValueError: size less than 0
+        """
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
