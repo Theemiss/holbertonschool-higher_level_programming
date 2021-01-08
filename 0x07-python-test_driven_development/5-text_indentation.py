23d7a7240dbdbf992bdd7341b85902cf6d843d17
Your Name <midinfotn401@gmail.com>
2021-01-08 22:13:34 +0100

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+    """
+
+
+def text_indentation(text):
+    """
+    print text
+    2 new lines after each of these characters: ., ? and :
+    Args:
+        text (str): text
+    Raise
+        TypeError: when text is not str
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    a = 0
+    while a < len(text):
+        if text[a] in [':', '.', '?']:
+            print(text[a])
+            print()
+            a += 1
+        else:
+            print(text[a], end='')
+        a += 1
