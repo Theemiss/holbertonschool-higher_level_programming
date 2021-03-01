c472bb7456ce19273b39e2455f9305d1d5225878
Themis <midinfotn401@gmail.com>
2021-03-01 14:56:14 +0000

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- CREATE SECOND TABLE and insert into
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256),score int);
+INSERT INTO second_table (id, name, score) value (1, 'John', 10);
+INSERT INTO second_table (id, name, score) value (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) value (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) value (4, 'George', 8);
+ 
