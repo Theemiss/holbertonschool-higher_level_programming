9660d85db0088a22517a533beedafd3dc9e8da8f
Themis <midinfotn401@gmail.com>
2021-03-02 10:23:47 +0000

    task 3 4

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- create a table 
+CREATE TABLE IF NOT EXISTS force_name (id int, name varchar(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- create a table
+CREATE TABLE IF NOT EXISTS id_not_null (id int DEFAULT 1, name varchar(256));
