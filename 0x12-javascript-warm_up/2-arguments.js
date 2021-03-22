2f9fea93a5b033815595ca7ce47c97a5be410772
Theemis <midinfotn401@gmail.com>
2021-03-22 13:35:07 +0100

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 let x = 0;
-process.argv.forEach((val, index) => {
+process.argv.forEach((_val, _index) => {
   x++;
 });
 if (x <= 2) {
   console.log('No argument');
+} else if (x === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
