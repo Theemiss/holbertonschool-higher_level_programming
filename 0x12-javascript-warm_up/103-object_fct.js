76b1572e3c9b4b0e9f4c51fb9ab01634e8fc2d39
Theemis <midinfotn401@gmail.com>
2021-03-22 15:59:17 +0100

    task 13

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+myObject.incr = function () {
+  return myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
