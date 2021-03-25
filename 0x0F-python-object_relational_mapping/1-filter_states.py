23486bc4d5d89dc155becafcfa73bcbda90224da
Themis <midinfotn401@gmail.com>
2021-03-25 15:41:58 +0100

    T1 fix T2 and datadump

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module list state where name start with N
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    query = "SELECT id,name FROM states WHERE name LIKE 'N%' ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
