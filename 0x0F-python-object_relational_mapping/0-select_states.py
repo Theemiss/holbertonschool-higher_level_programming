50528c6d37a5442e954e8942ebdfcf6ac7849408
Themis <midinfotn401@gmail.com>
2021-03-25 15:41:19 +0100

    T1 fix T2 and datadump

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,24 @@ import sys
 import MySQLdb
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db= sys.argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT id,name FROM states ORDER by id DESC")
-row = cur.fetchall()
-for r in row:
-	print (r)
-cur.close()
-conn.close()
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    query = "SELECT id,name FROM states ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
