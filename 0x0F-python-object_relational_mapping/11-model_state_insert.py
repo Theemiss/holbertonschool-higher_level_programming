d2770f386cc901b4faac272dfd2993de47691828
Themis <midinfotn401@gmail.com>
2021-03-28 11:45:35 +0100

    T 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    new_State = State(name="Louisiana")
+    re = session.add(new_State)
+    session.commit()
+    print(new_State.id)
+    session.close()
