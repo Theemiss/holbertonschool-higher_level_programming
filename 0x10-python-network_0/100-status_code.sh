9b5fcacfc89b948e8832280c27aadf3480285ad6
Themis <midinfotn401@gmail.com>
2021-04-08 15:25:32 +0100

    fix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#get statue code from http header 
-curl -sI "$1" | grep "HTTP/" |  cut -d " " -f 2
+#get statue code from http header  work just task(curl -sI "$1" | grep "HTTP/" |  cut -d " " -f 2)
+curl -so /dev/null --write-out "%{http_code}" "$1"
