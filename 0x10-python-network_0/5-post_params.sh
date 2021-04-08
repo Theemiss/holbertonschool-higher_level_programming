66cfa0fe0d7da29b070c9a56649ec4db5cad3456
Themis <midinfotn401@gmail.com>
2021-04-08 14:08:54 +0100

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request with custome data
-curl -s "$1" -X POST -d "email='hr@holbertonschool.com'&subject='I will always be here for PLD'"
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
