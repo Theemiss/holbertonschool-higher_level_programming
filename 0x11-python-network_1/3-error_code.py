cee7e87a4373c91df4164ff99d69ab5e4c14ecb4
Themis <midinfotn401@gmail.com>
2021-04-09 12:26:27 +0100

    T3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.error
+import sys
+"""
+request a body if error display statue code
+"""
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            info = response.read()
+            print(info.decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
