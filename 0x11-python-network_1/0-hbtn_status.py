9be1befbf3e3f25bf53a6183932486680e7bb990
Themis <midinfotn401@gmail.com>
2021-04-09 11:49:26 +0100

    init T0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(html.__class__))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('ascii')))
