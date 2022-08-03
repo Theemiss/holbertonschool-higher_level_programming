18fc477f21c1e5f798abc3d8e756c11b48760b05
Ahmed Belhaj <58787184+Theemiss@users.noreply.github.com>
2022-08-03 13:28:15 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const SquareA = require('./4-rectangle.js');
+const SquareA = require('./5-rectangle.js');
 module.exports = class Square extends SquareA {
   charPrint (c) {
     if (c === undefined) {
